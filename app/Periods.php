059565ec106d5c47fbc15451d8e307326762311d
Nikola <nidza.bg@gmail.com>
2020-07-15 03:45:57 +0200

    Added periods - might need more info.

---
+++ app/Periods.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Periods extends Model
+{
+    //
+}
---
+++ database/factories/PeriodsFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Periods;
+use Faker\Generator as Faker;
+
+$factory->define(Periods::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
