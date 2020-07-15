6af57d63649c514819a046d1c760440146d7c4da
Nikola <nidza.bg@gmail.com>
2020-07-15 03:41:51 +0200

    Added CMRF Apartments and Payments.

---
+++ database/migrations/2020_07_15_013627_create_payments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->integer("apartment_number");
+            $table->date("payment_date");
+            $table->integer("payment_month");
+            $table->integer("paid");
+            $table->integer("payment_method");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+}
